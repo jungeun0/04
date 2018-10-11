38880c3837f04a1700fc6f599e72e414c75188a3
jungeun0 <sky_fall7@naver.com>
2018-10-11 15:35:17 +0900

    04-2-2

--- main.c
+++ main.c
@@ -6,16 +6,20 @@
 int main(int argc, char *argv[]) {
 	int i;
 	
+	int a; //절댓값 저장하는 변수 
+	
 	printf("정수 하나를 입력하세요\n");
 	scanf("%d", &i);
 	
 	if (i>=0){
-		printf("절댓값은 %d입니다.\n", i);
+		a = i;
 		}	 
-	else if (i<0){
-		printf("절댓값은 %d입니다.\n", -i);
+	else {
+	
+		a = -i;
 	}
 	
+	printf("절댓값은 %d입니다.", a);
 
 	return 0;
 }
